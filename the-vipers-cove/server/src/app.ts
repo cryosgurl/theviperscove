d00d2b3de3f4dc37635586c7f6ffa649a5ea8f57
cryosgurl <mvzetten@meyn.com>
2018-12-21 15:59:24 +0100

    Added passport and able to talk to the DB

--- the-vipers-cove/server/src/app.ts
+++ the-vipers-cove/server/src/app.ts
@@ -5,7 +5,22 @@ import * as cors from 'cors';
 import * as bodyParser from 'body-parser';
 import * as mongoose from 'mongoose';
 
-import userRouter from './routes/users';
+import config from './config/database';
+import passportConfig from './config/passport';
+import users from './routes/users';
+
+// Connect to DataBase
+mongoose.connect(config.database);
+
+// Checks DB connection
+mongoose.connection.on('connected', () => {
+    console.log('Connected to DB ' + config.database);
+});
+
+// Checks On Error
+mongoose.connection.on('error', (err) => {
+    console.log('Failed to connect ' + err);
+});
 
 // Cors Middleware
 export const corsOptions = {
@@ -20,17 +35,22 @@ class App {
         this.app = express();
         this.config();
         this.routes();
+        this.passport();
+    }
+
+    private passport(): void {
+        this.app.use(passport.initialize());
+        this.app.use(passport.session());
+        passportConfig(passport);
     }
 
     private config(): void {
         // support application/json type post data
         this.app.use(bodyParser.json());
-        // support application/x-www-form-urlencoded post data
-        this.app.use(bodyParser.urlencoded({ extended: false }));
     }
 
     private routes(): void {
-        this.app.use(userRouter);
+        this.app.use('/users', users);
     }
 }
 
--- the-vipers-cove/server/src/routes/users.ts
+++ the-vipers-cove/server/src/routes/users.ts
@@ -1,11 +1,73 @@
 import * as express from 'express';
 import * as cors from 'cors';
-import { corsOptions } from '../app';
+import * as passport from 'passport';
+import * as jwt from 'jsonwebtoken';
 
-const userRouter = express.Router();
+import { User, addUser, getUserByUsername, comparePassword } from '../models/user';
+import { corsOptions } from '../app';
+import config from '../config/database';
+
+const router = express.Router();
 
 // Register
-userRouter.get('/register', cors(corsOptions), (req, res, next) => {
+router.post('/register', cors(corsOptions), (req, res, next) => {
+    const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    addUser(newUser, (err, user) => {
+        if (err) {
+            res.json({success: false, msg: 'failed to register user'});
+        } else {
+            res.json({success: true, msg: 'User registered'});
+        }
+    });
+});
+
+// Authenticate
+router.post('/authenticate', cors(corsOptions), (req, res, next) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    getUserByUsername(username, (err, user) => {
+        if (err) {
+            throw err;
+        }
+        if (!user) {
+            return res.json({success: false, msg: 'User not found'});
+        }
+
+        comparePassword(password, user.password, (error, isMatch) => {
+            if (error) {
+                throw error;
+            }
+            if (isMatch) {
+                const token = jwt.sign(user, config.secret, {
+                    expiresIn: 60400 // 1 week
+                });
+
+                res.json({
+                    success: true,
+                    token: 'JWT' + token,
+                    user: {
+                        id: user._id,
+                        name: user.name,
+                        username: user.username,
+                        email: user.email
+                    }
+                });
+            } else {
+                return res.json({success: false, msg: 'Wrong password'});
+            }
+        });
+    });
+});
+
+// profile
+router.get('/profile', cors(corsOptions), (req, res, next) => {
     res.send(
         {hello: [
             1, 2
@@ -13,4 +75,13 @@ userRouter.get('/register', cors(corsOptions), (req, res, next) => {
     );
 });
 
-export default userRouter;
+// validate
+router.get('/validate', cors(corsOptions), (req, res, next) => {
+    res.send(
+        {hello: [
+            1, 2
+        ]}
+    );
+});
+
+export default router;
